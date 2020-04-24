973cd3433b39f25a0574eb92cd93cc736858138f
Crowdbotics <team@crowdbotics.com>
2020-04-24 02:23:29 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/thai_sport_tuneups_16204/MainActivity.java
@@ -0,0 +1,15 @@
+package com.thai_sport_tuneups_16204;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "thai_sport_tuneups_16204";
+    }
+}
